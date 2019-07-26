097b3bbd3f73d9a06e52c6ac7072da35cdf1e547
iamfine420 <budagavichaitu@gmail.com>
2019-07-26 21:51:53 +0530

    Second commit

---
+++ condrend.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function UserGreeting(props){
+	return(
+		<h1>Hello World</h1>
+		);
+	}
+
+function GuestGreeting(props){
+	return(
+	<h1>Welcome to the Gues</h1>
+	)}
+
+
+function Greet(){
+	
+}
